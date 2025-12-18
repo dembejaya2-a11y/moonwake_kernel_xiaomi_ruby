7770a6346710ab4be54a1095f88b74a1577901ea
RainyXeon <rainyxeon@gmail.com>
2025-12-18 15:27:17 +0700

    FROMGIT: fs: add susfs_is_mnt_devname_ksu function back

--- fs/namespace.c
+++ fs/namespace.c
@@ -3941,6 +3941,20 @@ const struct proc_ns_operations mntns_operations = {
 	.owner		= mntns_owner,
 };
 
+#ifdef CONFIG_KSU_SUSFS
+bool susfs_is_mnt_devname_ksu(struct path *path) {
+	struct mount *mnt;
+
+	if (path && path->mnt) {
+		mnt = real_mount(path->mnt);
+		if (mnt && mnt->mnt_devname && !strcmp(mnt->mnt_devname, "KSU")) {
+			return true;
+		}
+	}
+	return false;
+}
+#endif
+
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 /* Reorder the mnt_id after all sus mounts are umounted during ksu_handle_setuid() */
 void susfs_reorder_mnt_id(void) {
